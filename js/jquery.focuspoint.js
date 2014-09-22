dbf9449ded209592630892c05ba513de03a7e270
g-van-vreckem <gvanvreckem@gmail.com>
2014-09-22 18:10:38 +0200

    Revert "Better video support typo"
    This reverts commit 9481f378090521c22e3a95e8e1ccca0946cfe212.

--- js/jquery.focuspoint.js
+++ js/jquery.focuspoint.js
@@ -123,7 +123,7 @@
 		var transform = ($el.data('transform') === 'true');
 		//Is it a video?
 		if ( $image.length == 0 ) {
-			$image = $el.find('video').first();
+			$image = $this.find('video').first();
 			transform = false;
 		}
 		
