ba550a84411dfcd6657132bafc81f27cdfad1543
Jonathon Menz <jonomenz@gmail.com>
2016-04-26 19:20:23 -0700

    Initial ideal cropping region support

--- js/jquery.focuspoint.js
+++ js/jquery.focuspoint.js
@@ -29,7 +29,7 @@
 			if (callNow) func.apply(context, args);
 		};
 	};
-	
+
 	// Resize throttling. Let requestAnimationFrame set framerate rather than using resize event rate which may be too rapid or too slow.
 	var running = false;
 	// Called on resize event
@@ -37,14 +37,15 @@
 		if (!running) {
 			running = true;
 			doResize();
-			debounce(function() {
-				// Not resizing anymore so cancel animation
-				running = false;
-			}, 250);
+			cancelResize();
 		}
 	};
+	var cancelResize = debounce(function() {
+		running = false;
+	}, 250);
 	var doResize = function() {
 		$resizeElements.focusPoint('adjustFocus');
+		// Repeat if still running
 		if (running) {
 			if (window.requestAnimationFrame) {
 				window.requestAnimationFrame(doResize);
@@ -65,6 +66,65 @@
 	// For sorting
 	var compareNumbers = function(a, b) {
 		return a - b;
+	};
+
+	// Calculate the left or top offset for an image on an axis
+	var getOffset = function(focusPos, scale) {
+		// How much bigger is the image than the container?
+		var spareSpace = scale - 1;
+		// If the image doesn't fill the container, position image on center
+		if (spareSpace < 0) {
+			return (1 - scale) * 0.5;
+		}
+		// Otherwise move image so focus point is as close to center of frame as possible
+		else if (spareSpace > 0) {
+			var negSpareSpace = spareSpace * -1;
+			var offset = 0.5 - (focusPos * scale);
+			if (offset > 0) {
+				return 0;
+			}
+			if (offset < 0 && offset < negSpareSpace) {
+				return negSpareSpace;
+			}
+			return offset;
+		}
+		return 0;
+	};
+
+	// Convert one or two coords in to an array of min,max and average values. If one coord is passed they will all be the same.
+	var getRange = function(axis, coordString, legacyGrid) {
+		var axisCoords = coordString.split(','),
+			axisRange = [];
+
+		// Make numeric and account for classic grid
+		$.each(axisCoords, function( index, value ) {
+			axisCoords[index] = parseFloat(value.trim());
+			if (legacyGrid === true) {
+				if (axis === 'X') {
+					axisCoords[index] = (axisCoords[index] + 1)*0.5;
+				}
+				else {
+					axisCoords[index] = (axisCoords[index] - 1)*-0.5;
+				}
+			}
+		});
+
+		// Set ranges
+		if (axisCoords.length > 1) {
+			axisCoords.sort(compareNumbers);
+			axisRange.start = axisCoords[0];
+			axisRange.stop = axisCoords[1];
+			// Average of min cropping region for classic focus point
+			axisRange.average = (axisCoords[0] + axisCoords[1]) / 2;
+		}
+		else {
+			// Same values for all
+			axisRange.start = axisRange.stop = axisRange.average = axisCoords[0];
+		}
+		// Ratio of range to image on this axis, inverted so we can determine the maximum allowable zoom level before the region will not all be viewable
+		axisRange.maxScale = 1 / (axisRange.stop - axisRange.start);
+
+		return axisRange;
 	};
 
 	// The FocusPoint plugin constructor
@@ -93,33 +153,26 @@
 			// There may be one or two focus coordinates per axis, defining a single point or area
 			var fp = this;
 			$.each(['X','Y'], function( index, axis ) {
-				fp['focus' + axis] = String(fp.$el.data('focus' + axis)).split(',');
-				// Make numeric
-				$.each(fp['focus' + axis], function( index, value ) {
-					fp['focus' + axis][index] = parseFloat(value.trim());
-					// Account for classic grid
-					if (fp.settings.legacyGrid === true) {
-						if (axis === 'X') {
-							fp['focus' + axis][index] = (fp['focus' + axis][index] + 1)*0.5;
-						}
-						else {
-							fp['focus' + axis][index] = (fp['focus' + axis][index] - 1)*-0.5;
-						}
-					}
-				});
-				// Set ranges
-				if (fp['focus' + axis].length > 1) {
-					fp['focus' + axis].sort(compareNumbers);
-					fp['focus' + axis + 'MinStart'] = fp['focus' + axis][0];
-					fp['focus' + axis + 'MinStop'] = fp['focus' + axis][1];
-					fp['focus' + axis] = (fp['focus' + axis][0] + fp['focus' + axis][1]) / 2;
-				}
-				else {
-					fp['focus' + axis + 'MinStart'] = fp['focus' + axis + 'MinStop'] = fp['focus' + axis] = fp['focus' + axis][0];
-				}
-				// Min cropping region ratios
-				fp['maxScaleRatio' + axis] = 1 / (fp['focus' + axis + 'MinStop'] - fp['focus' + axis + 'MinStart']);
+				// Get the main coordinates, including min cropping region if set
+				var axisRange = getRange(axis, String(fp.$el.data('focus' + axis)), fp.settings.legacyGrid);
+				fp['focus' + axis + 'MinStart'] = axisRange.start;
+				fp['focus' + axis + 'MinStop'] = axisRange.stop;
+				fp['focus' + axis] = axisRange.average;
+				fp['minCroppingRegionMaxScale' + axis] = axisRange.maxScale;
+
+				// Get the ideal region coordinates if set
+				axisRange = getRange(axis, String(fp.$el.data('focus' + axis + 'Ideal')), fp.settings.legacyGrid);
+				fp['focus' + axis + 'IdealStart'] = axisRange.start;
+				fp['focus' + axis + 'IdealStop'] = axisRange.stop;
+				fp['idealCroppingRegionMaxScale' + axis] = axisRange.maxScale;
 			});
+
+			this.minCroppingRegionW = (this.focusXMinStop - this.focusXMinStart) * this.imageW;
+			this.minCroppingRegionH = (this.focusYMinStop - this.focusYMinStart) * this.imageH;
+			this.minCroppingRegionRatio = this.minCroppingRegionW / this.minCroppingRegionH;
+			this.idealCroppingRegionW = (this.focusXIdealStop - this.focusXIdealStart) * this.imageW;
+			this.idealCroppingRegionH = (this.focusYIdealStop - this.focusYIdealStart) * this.imageH;
+			this.idealCroppingRegionRatio = this.idealCroppingRegionW / this.idealCroppingRegionH;
 
 			// Set transform origin
 			if (this.settings.setTransformOrigin) {
@@ -173,58 +226,81 @@
 			if (!(data.containerW > 0 && data.containerH > 0 && this.imageW > 0 && this.imageH > 0)) {
 				return false; //Need dimensions to proceed
 			}
-
 			data.containerRatio = data.containerW / data.containerH;
-			data.axisScale = {
-				X: this.imageRatio / data.containerRatio,
-				Y: data.containerRatio / this.imageRatio
-			};
-			data.clippingAxis = false;
-			// Scale and position image
-			if (this.imageRatio > data.containerRatio) {
-				data.clippingAxis = 'X';
-			}
-			else if (this.imageRatio < data.containerRatio) {
-				data.clippingAxis = 'Y';
-			}
-			else {
-				// No clipping
+
+			// Stop processing if the image and container aspect ratio match
+			if (data.containerRatio === this.imageRatio) {
 				this.$image.css({
 					'width': '100%',
 					'height': '100%',
 					'left': 0,
 					'top': 0
 				});
-				return; // no more processing needed
+				return;
+			}
+
+			// ToDo: this is weird
+			data.axisScale = {
+				X: this.imageRatio / data.containerRatio,
+				Y: data.containerRatio / this.imageRatio
+			};
+
+			// Determing the method of cropping to use
+			if (data.containerRatio >= this.idealCroppingRegionRatio && data.containerRatio <= (this.imageW / this.idealCroppingRegionH)) {
+				// Show entire ideal cropping range area, with extra image shown on the X axis
+				data.clippingMode = 'ideal';
+				data.primaryClippingAxis = 'Y';
+				data.secondaryClippingAxis = 'X';
+			}
+			else if (data.containerRatio <= this.idealCroppingRegionRatio && data.containerRatio >= (this.idealCroppingRegionW / this.imageH)) {
+				// Show entire ideal cropping range area, with extra image shown on the Y axis
+				data.clippingMode = 'ideal';
+				data.primaryClippingAxis = 'X';
+				data.secondaryClippingAxis = 'Y';
+			}
+			else if (this.imageRatio > data.containerRatio) {
+				// Show as much of the image as possible, cropping on the X axis
+				data.clippingMode = 'classic';
+				data.primaryClippingAxis = 'X';
+				if (data.axisScale.X > this.minCroppingRegionMaxScaleX) {
+					// Shrink the image to preserve the entire minimum cropping region, padding evenly on the Y axis
+					data.clippingMode = 'min';
+				}
+			}
+			else {
+				// Show as much of the image as possible, cropping on the Y axis
+				data.clippingMode = 'classic';
+				data.primaryClippingAxis = 'Y';
+				if (data.axisScale.Y > this.minCroppingRegionMaxScaleY) {
+					// Shrink the image to preserve the entire minimum cropping region, padding evenly on the X axis
+					data.clippingMode = 'min';
+				}
 			}
 
 			data.scale = 1;
 			data.shiftPrimary = 0;
 			data.shiftSecondary = 0;
-			if (this['maxScaleRatio' + data.clippingAxis] && data.axisScale[data.clippingAxis] > this['maxScaleRatio' + data.clippingAxis]) {
-				// Need to scale down image to fit min cropping region in frame
-				data.scale = this['maxScaleRatio' + data.clippingAxis] / data.axisScale[data.clippingAxis];
-				data.shiftSecondary = ((1 - data.scale) * 50) + '%';
-				data.shiftPrimary = (this['focus' + data.clippingAxis + 'MinStart'] * -100 * data.axisScale[data.clippingAxis] * data.scale) + '%';
+ 			if (data.clippingMode === 'ideal') {
+				// Scale up image to fit ideal cropping region in frame
+				data.scale = this['idealCroppingRegionMaxScale' + data.primaryClippingAxis] / data.axisScale[data.primaryClippingAxis];
+				data.shiftSecondary = (getOffset(this['focus' + data.secondaryClippingAxis], data.scale) * 100)  + '%';
+				data.shiftPrimary = (this['focus' + data.primaryClippingAxis + 'IdealStart'] * -100 * data.axisScale[data.primaryClippingAxis] * data.scale) + '%';
+			}
+ 			else if (data.clippingMode === 'min') {
+				// Scale down image to fit min cropping region in frame
+				data.scale = this['minCroppingRegionMaxScale' + data.primaryClippingAxis] / data.axisScale[data.primaryClippingAxis];
+				data.shiftSecondary = (getOffset(this['focus' + data.secondaryClippingAxis], data.scale) * 100)  + '%';
+				data.shiftPrimary = (this['focus' + data.primaryClippingAxis + 'MinStart'] * -100 * data.axisScale[data.primaryClippingAxis] * data.scale) + '%';
 			}
 			else {
-				// Move image so focus point is in center of frame
-				data.shiftPrimary = 0.5 - (this['focus' + data.clippingAxis] * data.axisScale[data.clippingAxis]);
-				// Make sure image fills frame
-				data.spareNeg = (data.axisScale[data.clippingAxis] - 1) * -1;
-				if (data.shiftPrimary > 0) {
-					data.shiftPrimary = 0;
-				}
-				else if (data.shiftPrimary < 0 && data.shiftPrimary < data.spareNeg) {
-					data.shiftPrimary = data.spareNeg;
-				}
-				data.shiftPrimary = (data.shiftPrimary * 100)  + '%';
+				// Show as much image as possible, with focus point as close to center as possible
+				data.shiftPrimary = (getOffset(this['focus' + data.primaryClippingAxis], data.axisScale[data.primaryClippingAxis]) * 100)  + '%';
 			}
 
 			// Assign CSS values to image container
-			if (data.clippingAxis === 'X') {
+			if (data.primaryClippingAxis === 'X') {
 				this.$image.css({
-					'width': ((data.axisScale[data.clippingAxis] * data.scale * 100) + '%'),
+					'width': ((data.axisScale[data.primaryClippingAxis] * data.scale * 100) + '%'),
 					'height': ((data.scale * 100) + '%'),
 					'left': (data.shiftPrimary),
 					'top': (data.shiftSecondary)
@@ -233,12 +309,11 @@
 			else { //clippingAxis === 'Y'
 				this.$image.css({
 					'width': ((data.scale * 100) + '%'),
-					'height': ((data.axisScale[data.clippingAxis] * data.scale * 100) + '%'),
+					'height': ((data.axisScale[data.primaryClippingAxis] * data.scale * 100) + '%'),
 					'left': (data.shiftSecondary),
 					'top': (data.shiftPrimary)
 				});
 			}
-
 		}
 	});
 
